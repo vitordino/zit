775db152740a382296eba11cd741fa2bf12f87a9
vitordino <me@vitordino.com>
2024-04-24 09:30:21 -0300

    :zap: only use loggerMiddleware on dev

--- src/main/middleware/index.ts
+++ src/main/middleware/index.ts
@@ -1,4 +1,5 @@
+import { is } from '@electron-toolkit/utils'
 import { gitMiddleware } from 'src/main/middleware/git'
 import { loggerMiddleware } from 'src/main/middleware/logger'
 
-export const middleware = [gitMiddleware, loggerMiddleware]
+export const middleware = is.dev ? [gitMiddleware, loggerMiddleware] : [gitMiddleware]
