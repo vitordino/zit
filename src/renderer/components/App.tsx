66cb5bf33a05afbdf5093ae5ff8fde5f9a48300e
vitordino <me@vitordino.com>
2024-04-26 07:14:55 -0300

    :fire: remove debug panel

--- src/renderer/components/App.tsx
+++ src/renderer/components/App.tsx
@@ -1,29 +1,11 @@
-import { useDispatch, useStore } from 'src/renderer/hooks/useStore'
 import { Header } from 'src/renderer/components/Header'
 import { FilePanel } from './FilePanel'
 
-export const App = () => {
-	const dispatch = useDispatch()
-	const state = useStore(x => x)
-	const getStatus = () => dispatch({ type: 'GIT:STATUS' })
-	const getBranches = () => dispatch({ type: 'GIT:BRANCH' })
-	return (
-		<>
-			<Header />
-			<main>
-				<div className='absolute h-screen w-1/2 overflow-auto border-border border-r bg-panel-background'>
-					<button className='block' onClick={getStatus}>
-						get status
-					</button>
-					<button className='block' onClick={getBranches}>
-						get branches
-					</button>
-					<pre className='select-all pointer-events-auto'>{JSON.stringify(state, null, 2)}</pre>
-				</div>
-				<div className='absolute h-screen w-1/2 left-1/2 overflow-auto'>
-					<FilePanel />
-				</div>
-			</main>
-		</>
-	)
-}
+export const App = () => (
+	<>
+		<Header />
+		<main>
+			<FilePanel />
+		</main>
+	</>
+)
--- src/renderer/components/Header.tsx
+++ src/renderer/components/Header.tsx
@@ -3,7 +3,7 @@ import { StatusBarButton } from 'src/renderer/components/Button'
 import { BranchSelector } from './BranchSelector'
 
 export const Header = () => (
-	<div className='relative pt-0.5 flex items-center bg-status-bar-background h-[35px] border-border border-b'>
+	<div className='relative pt-0.5 flex items-center bg-status-bar-background h-[35px]'>
 		<div className='drag-region h-full w-[76px]'></div>
 		{/* [TODO]: path selector (recent + open dialog) */}
 		<StatusBarButton>path</StatusBarButton>
