eb4fa05df58fab338b42d650e2bd7ed98bc1c8ac
vitordino <me@vitordino.com>
2024-04-26 12:08:49 -0300

    :refactor: use router

--- src/renderer/components/App.tsx
+++ src/renderer/components/App.tsx
@@ -1,16 +1,24 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { Header } from 'src/renderer/components/Header'
 import { FilePanel } from 'src/renderer/components/FilePanel'
 import { DebugWrapper } from 'src/renderer/components/DebugWrapper'
 import { CommitInput } from 'src/renderer/components/CommitInput'
-import { CommitLog } from './CommitLog'
+import { CommitLog } from 'src/renderer/components/CommitLog'
 
-export const App = () => (
-	<>
-		<DebugWrapper>
-			<Header />
-			<FilePanel />
-			<CommitInput />
-			<CommitLog />
-		</DebugWrapper>
-	</>
-)
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: (
+			<>
+				<DebugWrapper>
+					<Header />
+					<FilePanel />
+					<CommitInput />
+					<CommitLog />
+				</DebugWrapper>
+			</>
+		),
+	},
+])
+
+export const App = () => <RouterProvider router={router} />
