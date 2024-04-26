9791a1755634e135299300619774430ebc67255d
vitordino <me@vitordino.com>
2024-04-26 12:23:41 -0300

    :recycle: refactor commit line

--- src/renderer/components/CommitLine.tsx
+++ src/renderer/components/CommitLine.tsx
@@ -3,15 +3,16 @@ import { useSearchParams } from 'react-router-dom'
 import { CommitLineButton } from 'src/renderer/components/Button'
 import { CommitInput } from 'src/renderer/components/CommitInput'
 
+type CommitLineBaseProps = { toggleLog: () => void }
+export const CommitLineBase = ({ toggleLog }: CommitLineBaseProps) => (
+	<div className='flex'>
+		<CommitLineButton onClick={toggleLog}>log</CommitLineButton>
+		<CommitInput />
+	</div>
+)
+
 export const CommitLine = () => {
 	const [search, setSearch] = useSearchParams()
-
 	const toggleLog = () => setSearch({ log: search.get('log') === 'true' ? 'false' : 'true' })
-
-	return (
-		<div className='flex'>
-			<CommitLineButton onClick={toggleLog}>log</CommitLineButton>
-			<CommitInput />
-		</div>
-	)
+	return <CommitLineBase toggleLog={toggleLog} />
 }
