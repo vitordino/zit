18607e76be8ea6d3c41f172ab54b3f53e3f37f91
vitordino <me@vitordino.com>
2024-04-28 04:41:37 -0300

    :sparkles: show current path on header

--- src/renderer/components/Header.tsx
+++ src/renderer/components/Header.tsx
@@ -1,17 +1,21 @@
+import { useGitPath } from 'src/renderer/hooks/useStore'
 import { ThemeSelector } from 'src/renderer/components/ThemeSelector'
 import { StatusBarButton } from 'src/renderer/components/Button'
-import { BranchSelector } from './BranchSelector'
+import { BranchSelector } from 'src/renderer/components/BranchSelector'
 
-export const Header = () => (
-	<div className='flex-shrink-0 sticky top-0 w-full pt-0.5 flex items-center bg-status-bar-background h-[35px]'>
-		<div className='drag-region h-full w-[76px]'></div>
-		{/* [TODO]: path selector (recent + open dialog) */}
-		<StatusBarButton>path</StatusBarButton>
-		{/* [TODO]: branch selector (recent + local + remotes) */}
-		<BranchSelector />
-		<div className='drag-region h-full flex-1'></div>
-		<ThemeSelector />
-		<div className='drag-region h-full w-4'></div>
-		<div className='h-[1px] w-full absolute left-0 bottom-0 bg-border' />
-	</div>
-)
+export const Header = () => {
+	const path = useGitPath()
+	const folder = path.split('/').reverse()[0]
+	return (
+		<div className='flex-shrink-0 sticky top-0 w-full pt-0.5 flex items-center bg-status-bar-background h-[35px]'>
+			<div className='drag-region h-full w-[76px]'></div>
+			{/* [TODO]: path selector (recent + open dialog) */}
+			<StatusBarButton title={path}>{folder}</StatusBarButton>
+			<BranchSelector />
+			<div className='drag-region h-full flex-1'></div>
+			<ThemeSelector />
+			<div className='drag-region h-full w-4'></div>
+			<div className='h-[1px] w-full absolute left-0 bottom-0 bg-border' />
+		</div>
+	)
+}
