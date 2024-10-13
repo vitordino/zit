94297af21e4af1050b46d1670352162e61441585
vitordino <me@vitordino.com>
2024-10-13 11:47:23 -0300

    :bug: solve ariakit missing forwardRef issue

--- src/renderer/components/Button.tsx
+++ src/renderer/components/Button.tsx
@@ -1,4 +1,4 @@
-import type { ReactNode } from 'react'
+import { forwardRef, type ReactNode } from 'react'
 import * as Ariakit from '@ariakit/react'
 import { cn } from 'src/renderer/lib/css'
 import { Icon, type IconId } from 'src/renderer/components/Icon'
@@ -15,36 +15,44 @@ export const HeaderButton = ({ className, ...props }: ButtonProps) => (
 	/>
 )
 
-export const Button = ({ className, tooltip, ...props }: ButtonProps) => {
-	if (tooltip) {
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+	({ className, tooltip, ...props }, ref) => {
+		if (tooltip) {
+			return (
+				<Ariakit.TooltipProvider>
+					<Ariakit.TooltipAnchor
+						className={className}
+						render={x => <Button {...props} {...x} ref={ref} />}
+					/>
+					<Ariakit.Tooltip className='py-1 px-2 text-sm bg-panel-background border border-border-variant rounded-md'>
+						{tooltip}
+					</Ariakit.Tooltip>
+				</Ariakit.TooltipProvider>
+			)
+		}
 		return (
-			<Ariakit.TooltipProvider>
-				<Ariakit.TooltipAnchor
-					className={className}
-					render={<Button {...props} tooltip={false} />}
-				/>
-				<Ariakit.Tooltip className='py-1 px-2 text-sm bg-panel-background border border-border-variant rounded-md'>
-					{tooltip}
-				</Ariakit.Tooltip>
-			</Ariakit.TooltipProvider>
+			<button
+				ref={ref}
+				className={cn(
+					'disabled:text-text-disabled text-text leading-none text-xs outline-none p-1 hover:bg-background active:bg-element-active data-[state="highlight"]:text-text-accent focus-visible:bg-element-active ml-[1px] flex-shrink-0 rounded-md flex items-center cursor-pointer disabled:cursor-not-allowed',
+					className,
+				)}
+				{...props}
+			/>
 		)
-	}
-	return (
-		<button
-			className={cn(
-				'disabled:text-text-disabled text-text leading-none text-xs outline-none p-1 hover:bg-background active:bg-element-active data-[state="highlight"]:text-text-accent focus-visible:bg-element-active ml-[1px] flex-shrink-0 rounded-md flex items-center cursor-pointer disabled:cursor-not-allowed',
-				className,
-			)}
-			{...props}
-		/>
-	)
-}
+	},
+)
+
+Button.displayName = 'Button'
 
 type IconButtonState = 'default' | 'highlight'
 type IconButtonProps = ButtonProps & { iconId: IconId; state?: IconButtonState }
-export const IconButton = ({ iconId, children, state = 'default', ...props }: IconButtonProps) => (
-	<Button data-state={state} {...props}>
-		<Icon iconId={iconId} />
-		{!!children && <div className='ml-2 mr-1 -my-1'>{children}</div>}
-	</Button>
+export const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
+	({ iconId, children, state = 'default', ...props }, ref) => (
+		<Button data-state={state} {...props} ref={ref}>
+			<Icon iconId={iconId} />
+			{!!children && <div className='ml-2 mr-1 -my-1'>{children}</div>}
+		</Button>
+	),
 )
+IconButton.displayName = 'IconButton'
