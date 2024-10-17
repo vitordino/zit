e75c78479aa350a43c38c0fcbb23fe55717158d8
vitordino <me@vitordino.com>
2024-10-17 01:13:11 -0300

    :sparkles: add file system watch

--- src/main/window.ts
+++ src/main/window.ts
@@ -1,3 +1,4 @@
+import { subscribe } from '@parcel/watcher'
 import { shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { is } from '@electron-toolkit/utils'
@@ -5,9 +6,27 @@ import { is } from '@electron-toolkit/utils'
 import icon from 'resources/icon.png?asset'
 import { store } from './store'
 
-export const createWindow = ({ gitPath }: { gitPath: string }) => {
+const watchRepository = async (path: string) => {
+	const subscription = await subscribe(
+		path,
+		error => !error && store.dispatch({ type: 'GIT:REFRESH', path }),
+		{ ignore: ['.git'] },
+	)
+	return subscription.unsubscribe
+}
+
+const loadHTML = (window: BrowserWindow) => {
+	// HMR for renderer base on electron-vite cli.
+	// Load the remote URL for development or the local html file for production.
+	if (!is.dev || !process.env['ELECTRON_RENDERER_URL']) {
+		return window.loadFile(join(__dirname, '../renderer/index.html'))
+	}
+	return window.loadURL(process.env['ELECTRON_RENDERER_URL'])
+}
+
+export const createWindow = async ({ gitPath }: { gitPath: string }) => {
 	// Create the browser window.
-	const mainWindow = new BrowserWindow({
+	const window = new BrowserWindow({
 		width: 900,
 		height: 670,
 		show: false,
@@ -24,36 +43,32 @@ export const createWindow = ({ gitPath }: { gitPath: string }) => {
 			nodeIntegration: false,
 		},
 	})
-	mainWindow.setTitle(gitPath)
-	mainWindow.webContents.executeJavaScript(`
+	window.setTitle(gitPath)
+	window.webContents.executeJavaScript(`
 	  globalThis.sessionStorage.setItem('git-path', '${gitPath}')
 	`)
 
 	ipcMain.on('subscribe', async (state: unknown) => {
-		if (mainWindow?.isDestroyed()) return
-		mainWindow?.webContents?.send('subscribe', state)
+		if (window?.isDestroyed()) return
+		window?.webContents?.send('subscribe', state)
 	})
 
-	mainWindow.on('ready-to-show', () => {
+	window.on('ready-to-show', () => {
 		store.dispatch({ type: 'GIT:REFRESH', path: gitPath })
-		if (is.dev) return mainWindow.showInactive()
-		return mainWindow.show()
+		if (is.dev) return window.showInactive()
+		return window.show()
 	})
 
-	mainWindow.on('closed', () => {
+	loadHTML(window)
+
+	const unsubscribe = await watchRepository(gitPath)
+	window.on('closed', () => {
 		store.dispatch({ type: 'GIT:CLOSE', path: gitPath })
+		unsubscribe()
 	})
 
-	mainWindow.webContents.setWindowOpenHandler(details => {
+	window.webContents.setWindowOpenHandler(details => {
 		shell.openExternal(details.url)
 		return { action: 'deny' }
 	})
-
-	// HMR for renderer base on electron-vite cli.
-	// Load the remote URL for development or the local html file for production.
-	if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-		mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
-	} else {
-		mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
-	}
 }
